912a6ee48267e05ef3c89d607aefad195b1e56ce
Erin Flood <eflood@sfu.ca>
2021-04-01 15:01:59 -0700

    Create record declartions in header and definition shells in .cpp

--- record.cpp
+++ record.cpp
@@ -4,3 +4,82 @@
 #include <vector>
 
 using namespace std;
+
+    record::record(){
+
+    }
+    record::record(const record& other){
+
+    }
+
+    record::~record(){
+
+    }
+
+    int record::get_name() const{
+
+    }
+
+    int record::get_time() const{
+
+    }
+
+    int record::get_meal() const{
+
+    }
+
+    int record::get_ingreds() const{
+
+    }
+
+    int record::get_diet() const{
+
+    }
+
+    int record::get_ingred(int i) const{
+
+    }
+
+    int record::get_ingred_item(int i) const{
+
+    }
+
+    int record::get_ingred_amount(int i) const{
+
+    }
+
+    int record::set_name() const{
+
+    }
+
+    int record::set_time() const{
+
+    }
+
+    int record::set_meal() const{
+
+    }
+
+    int record::set_ingreds() const{
+
+    }
+
+    int record::set_diet() const{
+
+    }
+
+    int record::set_ingred(int i) const{
+
+    }
+
+    int record::set_ingred_item(int i) const{
+
+    }
+
+    int record::set_ingred_amount(int i) const{
+
+    }
+
+    void record::print() const{
+        
+    }
